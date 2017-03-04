cdf12149dd4faeaa71b82ef6ca5f62ce4e57db1a
rnb1984 <rnairnb@hotmail.com>
2017-03-04 10:54:27 +0000

    first commit

---
+++ api_site/api_app/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+# Class ObjectTest(TestCase):
+# def setup(self):
+# self.parameter = 'something'
+
+# def test_objt_function(self):
+# actual = result_of_func( self.parameter )
+# self.assertEquals(actual, 'expected')
